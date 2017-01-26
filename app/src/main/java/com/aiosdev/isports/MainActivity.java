1eff7f4b621b3e1bcc5f1ee95565a7ee70286a8d
suntaoas <suntaoas2014@gmail.com>
2017-01-26 12:22:48 -0500

    init app framwork

--- app/src/main/java/com/aiosdev/isports/MainActivity.java
+++ app/src/main/java/com/aiosdev/isports/MainActivity.java
@@ -1,13 +1,79 @@
 package com.aiosdev.isports;
 
+import android.graphics.Color;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.aiosdev.isports.tabmain.FirstLayerFragment;
+import com.shizhefei.view.indicator.FixedIndicatorView;
+import com.shizhefei.view.indicator.IndicatorViewPager;
+import com.shizhefei.view.indicator.transition.OnTransitionTextListener;
+import com.shizhefei.view.viewpager.SViewPager;
+
+public class MainActivity extends FragmentActivity {
+
+    private IndicatorViewPager indicatorViewPager;
+    private FixedIndicatorView indicator;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle arg0) {
+        super.onCreate(arg0);
         setContentView(R.layout.activity_main);
+        SViewPager viewPager = (SViewPager) findViewById(R.id.tabmain_viewPager);
+        indicator = (FixedIndicatorView) findViewById(R.id.tabmain_indicator);
+        indicator.setOnTransitionListener(new OnTransitionTextListener().setColor(Color.RED, Color.GRAY));
+
+        indicatorViewPager = new IndicatorViewPager(indicator, viewPager);
+        indicatorViewPager.setAdapter(new MyAdapter(getSupportFragmentManager()));
+        // 禁止viewpager的滑动事件
+        viewPager.setCanScroll(false);
+        // 设置viewpager保留界面不重新加载的页面数量
+        viewPager.setOffscreenPageLimit(4);
+    }
+
+    private class MyAdapter extends IndicatorViewPager.IndicatorFragmentPagerAdapter {
+        private String[] tabNames = {"计步", "统计", "地图", "计划"};
+        private int[] tabIcons = {R.drawable.maintab_1_selector, R.drawable.maintab_2_selector, R.drawable.maintab_3_selector,
+                R.drawable.maintab_4_selector};
+        private LayoutInflater inflater;
+
+        public MyAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+            inflater = LayoutInflater.from(getApplicationContext());
+        }
+
+        @Override
+        public int getCount() {
+            return tabNames.length;
+        }
+
+        @Override
+        public View getViewForTab(int position, View convertView, ViewGroup container) {
+            if (convertView == null) {
+                convertView = inflater.inflate(R.layout.tab_main, container, false);
+            }
+            TextView textView = (TextView) convertView;
+            textView.setText(tabNames[position]);
+            textView.setCompoundDrawablesWithIntrinsicBounds(0, tabIcons[position], 0, 0);
+            return textView;
+        }
+
+        @Override
+        public Fragment getFragmentForPage(int position) {
+            FirstLayerFragment mainFragment = new FirstLayerFragment();
+            Bundle bundle = new Bundle();
+            bundle.putString(FirstLayerFragment.INTENT_STRING_TABNAME, tabNames[position]);
+            bundle.putInt(FirstLayerFragment.INTENT_INT_INDEX, position);
+            mainFragment.setArguments(bundle);
+            return mainFragment;
+        }
     }
 }
