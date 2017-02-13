80729d85d5ebfe7f01cbeac9ed6edaf51032c481
suntaoas <suntaoas2014@gmail.com>
2017-02-13 14:21:17 -0500

    add function map Tab page

--- app/src/main/java/com/aiosdev/isports/fragments/FragmentTab3One.java
+++ app/src/main/java/com/aiosdev/isports/fragments/FragmentTab3One.java
@@ -1,27 +1,62 @@
 package com.aiosdev.isports.fragments;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.aiosdev.isports.R;
 import com.aiosdev.isports.tabmain.SecondLayerFragment;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.shizhefei.fragment.LazyFragment;
 import com.shizhefei.view.indicator.IndicatorViewPager;
 import com.shizhefei.view.indicator.IndicatorViewPager.IndicatorFragmentPagerAdapter;
 
-public class FragmentTab3One extends LazyFragment {
+import java.util.ArrayList;
+
+public class FragmentTab3One extends LazyFragment implements LocationListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener , OnMapReadyCallback {
 	private IndicatorViewPager indicatorViewPager;
 	private LayoutInflater inflate;
 	public static final String INTENT_STRING_TABNAME = "intent_String_tabName";
 	public static final String INTENT_INT_POSITION = "intent_int_position";
 	private String tabName;
 	private int index;
+
+	private GoogleMap mMap;
+	private GoogleApiClient mGoogleApiClient;
+	private LocationRequest locationRequest;
+
+	private PolylineOptions mPolylineOptions;
+	private ArrayList<Location> mLocations;
+
+	private int i = 0;
+
+	private Button btStart;
+	private Button btStop;
 
 	@Override
 	protected void onCreateViewLazy(Bundle savedInstanceState) {
@@ -33,45 +68,136 @@ public class FragmentTab3One extends LazyFragment {
 		tabName = bundle.getString(INTENT_STRING_TABNAME);
 		index = bundle.getInt(INTENT_INT_POSITION);
 
-		/*
-		ViewPager viewPager = (ViewPager) findViewById(R.id.fragment_tabmain_viewPager);
-		Indicator indicator = (Indicator) findViewById(R.id.fragment_tabmain_indicator);
+		// Obtain the SupportMapFragment and get notified when the map is ready to be used.
+		SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map3);
+		mapFragment.getMapAsync(FragmentTab3One.this);
 
-		switch (index) {
-		case 0:
-			indicator.setScrollBar(new ColorBar(getApplicationContext(), Color.RED, 5));
-			break;
-		case 1:
-			indicator.setScrollBar(new ColorBar(getApplicationContext(), Color.RED, 0, Gravity.CENTENT_BACKGROUND));
-			break;
-		case 2:
-			indicator.setScrollBar(new ColorBar(getApplicationContext(), Color.RED, 5, Gravity.TOP));
-			break;
-		case 3:
-			indicator.setScrollBar(new LayoutBar(getApplicationContext(), R.layout.layout_slidebar, Gravity.CENTENT_BACKGROUND));
-			break;
-		}
+		btStart = (Button) findViewById(R.id.bt_tab3_one_start);
+		btStop = (Button) findViewById(R.id.bt_tab3_one_stop);
+		btStart.setEnabled(false);
+		btStop.setEnabled(false);
 
-		float unSelectSize = 16;
-		float selectSize = unSelectSize * 1.2f;
-
-		int selectColor = res.getColor(R.color.tab_top_text_2);
-		int unSelectColor = res.getColor(R.color.tab_top_text_1);
-		indicator.setOnTransitionListener(new OnTransitionTextListener().setColor(selectColor, unSelectColor).setSize(selectSize, unSelectSize));
-
-		viewPager.setOffscreenPageLimit(4);
-
-		indicatorViewPager = new IndicatorViewPager(indicator, viewPager);
-		inflate = LayoutInflater.from(getApplicationContext());
-
-		// 注意这里 的FragmentManager 是 getChildFragmentManager(); 因为是在Fragment里面
-		// 而在activity里面用FragmentManager 是 getSupportFragmentManager()
-		indicatorViewPager.setAdapter(new MyAdapter(getChildFragmentManager()));
-
-		Log.d("cccc", "Fragment 将要创建View " + this);
-		*/
 	}
 
+	@Override
+	public void onMapReady(GoogleMap map) {
+		mMap = map;
+
+		mMap.setIndoorEnabled(true);
+
+		if (ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+			// TODO: Consider calling
+			//    ActivityCompat#requestPermissions
+			// here to request the missing permissions, and then overriding
+			//   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+			//                                          int[] grantResults)
+			// to handle the case where the user grants the permission. See the documentation
+			// for ActivityCompat#requestPermissions for more details.
+			return;
+		}
+		mMap.setMyLocationEnabled(true);
+
+		//首先生成一个GoogleApiClient对象并且设置属性
+		if (mGoogleApiClient == null) {
+			mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+					.addConnectionCallbacks(this)
+					.addOnConnectionFailedListener(this)
+					.addApi(LocationServices.API)
+					.build();
+		}
+
+		//然后连接,连接成功后会在onConnected回调
+		if (mGoogleApiClient != null) {
+			mGoogleApiClient.connect();
+		}
+	}
+
+	@Override
+	protected void onDestroyViewLazy() {
+		super.onDestroyViewLazy();
+
+		//停止定位
+		stopLocation();
+
+		//设置start按钮可用
+		btStart.setEnabled(true);
+		btStop.setEnabled(false);
+	}
+
+	@Override
+	public void onConnected(@Nullable Bundle bundle) {
+		if (ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+			// TODO: Consider calling
+			//    ActivityCompat#requestPermissions
+			// here to request the missing permissions, and then overriding
+			//   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+			//                                          int[] grantResults)
+			// to handle the case where the user grants the permission. See the documentation
+			// for ActivityCompat#requestPermissions for more details.
+			return;
+		}
+
+		Location myLocation =
+				LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+		if (myLocation != null) {
+			LatLng myPoint = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
+			mMap.addMarker(new MarkerOptions().position(myPoint).title("Current Location"));
+			mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myPoint, 17));
+		}
+
+		btStart.setEnabled(true);
+		btStart.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				//开始定位前构造LocationRequest类设置定位时的属性.比如隔多久定位一次，设置定位精度等等
+				createLocationRequest();
+
+				//创建完成后开始定位
+				startLocation();
+
+				//设置stop按钮可用
+				btStart.setEnabled(false);
+				btStop.setEnabled(true);
+			}
+		});
+
+		btStop.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+
+				//停止定位
+				stopLocation();
+
+				//设置start按钮可用
+				btStart.setEnabled(true);
+				btStop.setEnabled(false);
+			}
+		});
+
+
+	}
+
+	@Override
+	public void onConnectionSuspended(int i) {
+
+	}
+
+	@Override
+	public void onConnectionFailed(@NonNull ConnectionResult result) {
+
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+
+		mMap.clear();
+		mMap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(), location.getLongitude())));
+
+		mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 17));
+		Toast.makeText(getActivity(), "新位置被检测" + i++ , Toast.LENGTH_SHORT).show();
+		Log.d("新位置：", i+"");
+
+	}
 
 
 	private class MyAdapter extends IndicatorFragmentPagerAdapter {
@@ -105,5 +231,50 @@ public class FragmentTab3One extends LazyFragment {
 			return mainFragment;
 		}
 	}
+
+	private void createLocationRequest() {
+		locationRequest = new LocationRequest();
+		locationRequest.setInterval(10000);
+		locationRequest.setFastestInterval(1000);
+		locationRequest.setSmallestDisplacement(0.1F);
+		locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+	}
+
+	public void startLocation() {
+		Log.d("mGoogleApiClient是否链接:", mGoogleApiClient.isConnected() + "");
+		if (mGoogleApiClient.isConnected()) {
+			if (ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+				// TODO: Consider calling
+				//    ActivityCompat#requestPermissions
+				// here to request the missing permissions, and then overriding
+				//   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+				//                                          int[] grantResults)
+				// to handle the case where the user grants the permission. See the documentation
+				// for ActivityCompat#requestPermissions for more details.
+				return;
+			}
+			LocationServices.FusedLocationApi.requestLocationUpdates(
+					mGoogleApiClient, locationRequest, this);
+			Log.d("位置变化监听器状态：", "开启！");
+		}
+	}
+	public void stopLocation() {
+		Log.d("mGoogleApiClient是否链接:", mGoogleApiClient.isConnected() + "");
+		if (mGoogleApiClient.isConnected()) {
+			if (ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+				// TODO: Consider calling
+				//    ActivityCompat#requestPermissions
+				// here to request the missing permissions, and then overriding
+				//   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+				//                                          int[] grantResults)
+				// to handle the case where the user grants the permission. See the documentation
+				// for ActivityCompat#requestPermissions for more details.
+				return;
+			}
+			LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+		}
+	}
+
 
 }
--- app/src/main/java/com/aiosdev/isports/fragments/FragmentTab3Two.java
+++ app/src/main/java/com/aiosdev/isports/fragments/FragmentTab3Two.java
@@ -1,109 +1,161 @@
 package com.aiosdev.isports.fragments;
 
+import android.content.Intent;
 import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.text.Html;
+import android.text.Spanned;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.aiosdev.isports.R;
+import com.aiosdev.isports.adapter.PlaceAutocompleteAdapter;
 import com.aiosdev.isports.tabmain.SecondLayerFragment;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.places.AutocompletePrediction;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceBuffer;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.shizhefei.fragment.LazyFragment;
 import com.shizhefei.view.indicator.IndicatorViewPager;
 import com.shizhefei.view.indicator.IndicatorViewPager.IndicatorFragmentPagerAdapter;
 
-public class FragmentTab3Two extends LazyFragment {
-	private IndicatorViewPager indicatorViewPager;
-	private LayoutInflater inflate;
-	public static final String INTENT_STRING_TABNAME = "intent_String_tabName";
-	public static final String INTENT_INT_POSITION = "intent_int_position";
-	private String tabName;
-	private int index;
+public class FragmentTab3Two extends LazyFragment implements GoogleApiClient.OnConnectionFailedListener {
+    private IndicatorViewPager indicatorViewPager;
+    private LayoutInflater inflate;
+    public static final String INTENT_STRING_TABNAME = "intent_String_tabName";
+    public static final String INTENT_INT_POSITION = "intent_int_position";
+    private String tabName;
+    private int index;
+    private EditText et_start;
+    private EditText et_dest;
+    private boolean bt_navigate_Value;
+    private Button bt_navigate;
 
-	@Override
-	protected void onCreateViewLazy(Bundle savedInstanceState) {
-		super.onCreateViewLazy(savedInstanceState);
-		setContentView(R.layout.fragment_tab_3_two);
-		Resources res = getResources();
+    protected GoogleApiClient mGoogleApiClient;
 
-		Bundle bundle = getArguments();
-		tabName = bundle.getString(INTENT_STRING_TABNAME);
-		index = bundle.getInt(INTENT_INT_POSITION);
+    private PlaceAutocompleteAdapter mAdapter;
 
-		/*
-		ViewPager viewPager = (ViewPager) findViewById(R.id.fragment_tabmain_viewPager);
-		Indicator indicator = (Indicator) findViewById(R.id.fragment_tabmain_indicator);
+    private AutoCompleteTextView mAutocompleteViewStart;
+    private AutoCompleteTextView mAutocompleteViewStop;
 
-		switch (index) {
-		case 0:
-			indicator.setScrollBar(new ColorBar(getApplicationContext(), Color.RED, 5));
-			break;
-		case 1:
-			indicator.setScrollBar(new ColorBar(getApplicationContext(), Color.RED, 0, Gravity.CENTENT_BACKGROUND));
-			break;
-		case 2:
-			indicator.setScrollBar(new ColorBar(getApplicationContext(), Color.RED, 5, Gravity.TOP));
-			break;
-		case 3:
-			indicator.setScrollBar(new LayoutBar(getApplicationContext(), R.layout.layout_slidebar, Gravity.CENTENT_BACKGROUND));
-			break;
-		}
+    private TextView mPlaceDetailsAttribution;
 
-		float unSelectSize = 16;
-		float selectSize = unSelectSize * 1.2f;
+    private static final LatLngBounds BOUNDS_GREATER_SYDNEY = new LatLngBounds(
+            new LatLng(-34.041458, 150.790100), new LatLng(-33.682247, 151.383362));
 
-		int selectColor = res.getColor(R.color.tab_top_text_2);
-		int unSelectColor = res.getColor(R.color.tab_top_text_1);
-		indicator.setOnTransitionListener(new OnTransitionTextListener().setColor(selectColor, unSelectColor).setSize(selectSize, unSelectSize));
+    @Override
+    protected void onCreateViewLazy(Bundle savedInstanceState) {
+        super.onCreateViewLazy(savedInstanceState);
 
-		viewPager.setOffscreenPageLimit(4);
+        mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+                .enableAutoManage(getActivity(), 0 /* clientId */, this)
+                .addApi(Places.GEO_DATA_API)
+                .build();
 
-		indicatorViewPager = new IndicatorViewPager(indicator, viewPager);
-		inflate = LayoutInflater.from(getApplicationContext());
+        setContentView(R.layout.fragment_tab_3_two);
+        Resources res = getResources();
 
-		// 注意这里 的FragmentManager 是 getChildFragmentManager(); 因为是在Fragment里面
-		// 而在activity里面用FragmentManager 是 getSupportFragmentManager()
-		indicatorViewPager.setAdapter(new MyAdapter(getChildFragmentManager()));
+        // Retrieve the AutoCompleteTextView that will display Place suggestions.
+        mAutocompleteViewStart = (AutoCompleteTextView)
+                findViewById(R.id.et_tab3_two_search2);
 
-		Log.d("cccc", "Fragment 将要创建View " + this);
-		*/
-	}
+        mAutocompleteViewStop = (AutoCompleteTextView)
+                findViewById(R.id.et_tab3_two_search3);
+
+        // Register a listener that receives callbacks when a suggestion has been selected
+        //mAutocompleteViewStart.setOnItemClickListener(mAutocompleteClickListener);
+        //mAutocompleteViewStop.setOnItemClickListener(mAutocompleteClickListener);
+
+        // Retrieve the TextViews that will display details and attributions of the selected place.
+        //mPlaceDetailsText = (TextView) findViewById(R.id.place_details);
+        //mPlaceDetailsAttribution = (TextView) findViewById(R.id.place_attribution);
+
+        // Set up the adapter that will retrieve suggestions from the Places Geo Data API that cover
+        // the entire world.
+        mAdapter = new PlaceAutocompleteAdapter(getActivity(), mGoogleApiClient, BOUNDS_GREATER_SYDNEY,
+                null);
+        mAutocompleteViewStart.setAdapter(mAdapter);
+        mAutocompleteViewStop.setAdapter(mAdapter);
 
 
 
-	private class MyAdapter extends IndicatorFragmentPagerAdapter {
+        Bundle bundle = getArguments();
+        tabName = bundle.getString(INTENT_STRING_TABNAME);
+        index = bundle.getInt(INTENT_INT_POSITION);
 
-		public MyAdapter(FragmentManager fragmentManager) {
-			super(fragmentManager);
-		}
+        bt_navigate = (Button) findViewById(R.id.bt_tab3_two_go2);
 
-		@Override
-		public int getCount() {
-			return 5;
-		}
+        bt_navigate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Navigation", "导航开始");
+                String str1 = mAutocompleteViewStart.getText().toString();
+                String str2 = mAutocompleteViewStop.getText().toString();
+                Intent localIntent = new Intent("android.intent.action.VIEW", Uri.parse("http://maps.google.com/maps?saddr=" + str2 + "&daddr=" + str1));
+                localIntent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
+                startActivity(localIntent);
 
-		@Override
-		public View getViewForTab(int position, View convertView, ViewGroup container) {
-			if (convertView == null) {
-				convertView = inflate.inflate(R.layout.tab_top, container, false);
-			}
-			TextView textView = (TextView) convertView;
-			textView.setText(tabName + " " + position);
-			return convertView;
-		}
+            }
+        });
 
-		@Override
-		public Fragment getFragmentForPage(int position) {
-			SecondLayerFragment mainFragment = new SecondLayerFragment();
-			Bundle bundle = new Bundle();
-			bundle.putString(SecondLayerFragment.INTENT_STRING_TABNAME, tabName);
-			bundle.putInt(SecondLayerFragment.INTENT_INT_POSITION, position);
-			mainFragment.setArguments(bundle);
-			return mainFragment;
-		}
-	}
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+        // TODO(Developer): Check error code and notify the user of error state and resolution.
+        Toast.makeText(getActivity(),
+                "Could not connect to Google API Client: Error " + connectionResult.getErrorCode(),
+                Toast.LENGTH_SHORT).show();
+    }
+
+    private class MyAdapter extends IndicatorFragmentPagerAdapter {
+
+        public MyAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+
+        @Override
+        public int getCount() {
+            return 5;
+        }
+
+        @Override
+        public View getViewForTab(int position, View convertView, ViewGroup container) {
+            if (convertView == null) {
+                convertView = inflate.inflate(R.layout.tab_top, container, false);
+            }
+            TextView textView = (TextView) convertView;
+            textView.setText(tabName + " " + position);
+            return convertView;
+        }
+
+        @Override
+        public Fragment getFragmentForPage(int position) {
+            SecondLayerFragment mainFragment = new SecondLayerFragment();
+            Bundle bundle = new Bundle();
+            bundle.putString(SecondLayerFragment.INTENT_STRING_TABNAME, tabName);
+            bundle.putInt(SecondLayerFragment.INTENT_INT_POSITION, position);
+            mainFragment.setArguments(bundle);
+            return mainFragment;
+        }
+    }
 
 }
